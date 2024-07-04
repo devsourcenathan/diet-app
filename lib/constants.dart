8dff46ad5fd2e1299662a91abe5759fb48b1de2e
Nathan Tchinda <sadenachbinathan@gmail.com>
2024-07-04 14:14:15 +0100

    Update styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Constants {
   //Primary color
-  static var primaryColor = Color.fromARGB(255, 16, 219, 46);
+  static var primaryColor = Color.fromARGB(255, 29, 158, 48);
   static var blackColor = Colors.black54;
 
   //Onboarding texts
